093d2f49aca5b6cf32b4f8f2952c9510ff88ba04
Thomas Pun <thomas@pun.io>
2014-09-05 00:07:35 +0800

    initial commit

---
+++ FindMyPhone/FindMyPhoneTests/FindMyPhoneTests.m
@@ -0,0 +1,34 @@
+//
+//  FindMyPhoneTests.m
+//  FindMyPhoneTests
+//
+//  Created by Thomas Pun on 9/5/14.
+//  Copyright (c) 2014 Loop Pulse. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FindMyPhoneTests : XCTestCase
+
+@end
+
+@implementation FindMyPhoneTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
